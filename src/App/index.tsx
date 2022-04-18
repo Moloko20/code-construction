9eccd02c8e49ecf965879957deb50744ffc98ec6
Moloko20 <alexvfrontend@gmail.com>
2022-04-18 14:05:49 +0300

    feat(app): add redux

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,26 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { store } from 'store'
+
+import { getUsers } from 'services/getUsers'
+
+import { useAppDispatch } from 'hooks'
+
+import { UsersActionTypes } from 'types/users'
 
 import { Menu } from 'components/Menu'
 import { UsersList } from 'components/UsersList'
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    React.useEffect(() => {
+        getUsers().then(data => dispatch({ type: UsersActionTypes.FETCH_USERS, payload: data }))
+    }, [])
+
     require('./index.sass')
 
     return (
@@ -31,8 +46,10 @@ function App() {
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </StrictMode>,
 )
