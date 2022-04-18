0ddc0af9b8cd8c64b2ee671a701aff40478ca7b0
Moloko20 <alexvfrontend@gmail.com>
2022-04-18 21:03:56 +0300

    feat(types): add users action type - set_users

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,9 +1,13 @@
 export enum UsersActionTypes {
     FETCH_USERS = 'FETCH_USERS',
     GET_USERS = 'GET_USERS',
+    SET_USERS = 'SET_USERS',
 }
 
-export type UsersActionType = UsersActionTypes.FETCH_USERS | UsersActionTypes.GET_USERS
+export type UsersActionType =
+    | UsersActionTypes.FETCH_USERS
+    | UsersActionTypes.GET_USERS
+    | UsersActionTypes.SET_USERS
 
 export interface UsersAction {
     type: UsersActionType
